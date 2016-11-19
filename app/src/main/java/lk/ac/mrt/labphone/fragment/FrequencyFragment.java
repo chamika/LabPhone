52cecba3626ea1d4a160f6f0132c4cf235fcd2d0
xwpdev <charith.suriyakula@live.com>
2016-11-19 09:54:16 +0530

    Added Zentone and UI + tone generatot updated

--- app/src/main/java/lk/ac/mrt/labphone/fragment/FrequencyFragment.java
+++ app/src/main/java/lk/ac/mrt/labphone/fragment/FrequencyFragment.java
@@ -1,6 +1,6 @@
 package lk.ac.mrt.labphone.fragment;
 
-
+import android.annotation.TargetApi;
 import android.media.AudioFormat;
 import android.media.AudioManager;
 import android.media.AudioTrack;
@@ -12,13 +12,21 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CompoundButton;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Toast;
 import android.widget.ToggleButton;
 
+import in.excogitation.zentone.library.ToneStoppedListener;
+import in.excogitation.zentone.library.ZenTone;
+import lk.ac.mrt.labphone.MainActivity;
 import lk.ac.mrt.labphone.R;
 
 import static android.R.attr.duration;
 import static android.R.attr.id;
+import static android.R.attr.x;
 import static android.R.id.toggle;
+import static android.icu.lang.UCharacter.GraphemeClusterBreak.V;
 import static android.widget.CompoundButton.*;
 import static lk.ac.mrt.labphone.R.string.frequency;
 
@@ -26,73 +34,148 @@ import static lk.ac.mrt.labphone.R.string.frequency;
  * A simple {@link Fragment} subclass.
  */
 public class FrequencyFragment extends Fragment {
-
+    private int freq = 5000;
+    private int duration = 1;
+    private boolean isPlaying = false;
+    ToggleButton toggle;
 
     public FrequencyFragment() {
         // Required empty public constructor
     }
-
-    AudioTrack mAudioTrack;
-    boolean isPlaying = false;
 
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View v = inflater.inflate(R.layout.fragment_frequency, container, false);
+        final View v = inflater.inflate(R.layout.fragment_frequency, container, false);
 
-        ToggleButton toggle = (ToggleButton) v.findViewById(R.id.toggleButtonFrequency);
+        // inputs
+        SeekBar frequencySeek = (SeekBar) v.findViewById(R.id.seekBarFrequency);
+        frequencySeek.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                TextView freqLabel = (TextView) v.findViewById(R.id.textViewFrequencyNo);
+                freqLabel.setText(String.valueOf(progress));
+                freq = progress;
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                ZenTone.getInstance().stop();
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        SeekBar frequencyDuration = (SeekBar) v.findViewById(R.id.seekBarDuration);
+        frequencyDuration.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                TextView durLabel = (TextView) v.findViewById(R.id.textViewDurationNo);
+                durLabel.setText(String.valueOf(progress));
+                duration = progress;
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                ZenTone.getInstance().stop();
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        // sound generator
+        toggle = (ToggleButton) v.findViewById(R.id.toggleButtonFrequency);
         toggle.setOnCheckedChangeListener(new OnCheckedChangeListener() {
             @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 if (isChecked) {
+                    // isPlaying = isChecked;
                     // The toggle is enabled
-                    playSound(220);
+                    // playSound(1500);
+                    handleTonePlay("1500", "5");
                 } else {
                     // The toggle is disabled
-                    stopSound(mAudioTrack);
+                    // stopSound(mAudioTrack);
+                    ZenTone.getInstance().stop();
                 }
             }
         });
         return v;
     }
 
-    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
-    private void playSound(double frequency) {
+    private void handleTonePlay(String freqString, String durationString) {
+        if (!"".equals(freqString) && !"".equals(durationString)) {
+            if (!isPlaying) {
+                freq = Integer.parseInt(freqString);
+                duration = Integer.parseInt(durationString);
+                // Play Tone
+                ZenTone.getInstance().generate(freq, duration, 0.5f, new ToneStoppedListener() {
+                    @Override
+                    public void onToneStopped() {
+                        toggle.setChecked(false);
+                        isPlaying = false;
+                    }
+                });
 
-        // initialize audio
-        // AudioTrack definition
-        int mBufferSize = AudioTrack.getMinBufferSize(44100,
-                AudioFormat.CHANNEL_OUT_MONO,
-                AudioFormat.ENCODING_PCM_8BIT);
-
-        // Sine wave
-        final double[] mSound = new double[44100];
-        final short[] mBuffer = new short[duration];
-        for (int i = 0; i < mSound.length; i++) {
-            mSound[i] = Math.sin((2.0 * Math.PI * i / (44100 / frequency)));
-            mBuffer[i] = (short) (mSound[i] * Short.MAX_VALUE);
-        }
-
-        mAudioTrack = new AudioTrack(AudioManager.STREAM_MUSIC, 44100,
-                AudioFormat.CHANNEL_OUT_MONO, AudioFormat.ENCODING_PCM_16BIT,
-                mBufferSize, AudioTrack.MODE_STREAM);
-
-        mAudioTrack.setVolume(1.0f);
-
-        mAudioTrack.play();
-
-        isPlaying = true;
-
-        mAudioTrack.write(mBuffer, 0, mSound.length);
-    }
-
-    private void stopSound(AudioTrack mAudioTrack) {
-        isPlaying = false;
-        if (mAudioTrack != null) {
-            mAudioTrack.stop();
-            mAudioTrack.release();
+                isPlaying = true;
+            } else {
+                // Stop Tone
+                ZenTone.getInstance().stop();
+                isPlaying = false;
+            }
+        } else if ("".equals(freqString)) {
+//            Toast.makeText(MainActivity.this, "Please enter a frequency!", Toast.LENGTH_SHORT).show();
+        } else if ("".equals(durationString)) {
+//            Toast.makeText(MainActivity.this, "Please enter duration!", Toast.LENGTH_SHORT).show();
         }
     }
+
+// legacy method
+//    @TargetApi(Build.VERSION_CODES.CUPCAKE)
+//    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+//    private void playSound(double frequency) {
+//        while (isPlaying) {
+//            // initialize audio
+//            // AudioTrack definition
+//            int mBufferSize = AudioTrack.getMinBufferSize(44100,
+//                    AudioFormat.CHANNEL_OUT_MONO,
+//                    AudioFormat.ENCODING_PCM_8BIT);
+//
+//            // Sine wave
+//            final double[] mSound = new double[44100];
+//            final short[] mBuffer = new short[duration];
+//            for (int i = 0; i < mSound.length; i++) {
+//                mSound[i] = Math.sin((2.0 * Math.PI * i / (44100 / frequency)));
+//                mBuffer[i] = (short) (mSound[i] * Short.MAX_VALUE);
+//
+//            }
+//            mAudioTrack = new AudioTrack(AudioManager.STREAM_MUSIC, 44100,
+//                    AudioFormat.CHANNEL_OUT_MONO, AudioFormat.ENCODING_PCM_16BIT,
+//                    mBufferSize, AudioTrack.MODE_STREAM);
+//            mAudioTrack.play();
+//
+//            mAudioTrack.write(mBuffer, 0, mSound.length);
+//
+//            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+//                mAudioTrack.setVolume(1.0f);
+//            }
+//        }
+//    }
+//
+//    private void stopSound(AudioTrack mAudioTrack) {
+//        isPlaying = false;
+//        if (mAudioTrack != null) {
+//            mAudioTrack.stop();
+//            mAudioTrack.release();
+//        }
+//    }
 }
